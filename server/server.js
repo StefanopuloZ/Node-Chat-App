2dfdb4353c38cc1ea084769e17c935aa8dc862a8
Stefan Deak <deakstefan@gmail.com>
2019-02-02 15:25:44 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is listnening on port ${port}`);
+});
+
+module.exports = { app };
+
+
+
+
